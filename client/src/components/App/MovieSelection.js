d70c166a34fd6d02be3ff1b35a82744f36844bb0
teobaseballer <49565300+teobaseballer@users.noreply.github.com>
2023-06-26 02:20:30 +0000

    completed

--- client/src/components/App/MovieSelection.js
+++ client/src/components/App/MovieSelection.js
@@ -1,19 +1,40 @@
 import * as React from 'react';
-//import all necessary libraries here, e.g., Material-UI Typography, as follows
-import Typography from '@mui/material/Typography';
 
-const MovieSelection = () => {
+//MUI Select
+import Select from "@mui/material/Select";
 
-  //states declarations
-  //constants and functions declarations
+//MUI Menu Item
+import MenuItem from "@mui/material/MenuItem";
 
+//MUI Typography Element
+import Typography from "@mui/material/Typography";
+
+const MovieSelection = ({ movies, selectedMovie, handleMovieChange, showSelectError }) => {
   return (
-    <>
-    
-    {/* JSX block */}
-
-
-    </>
+    <div>
+      <Typography variant="subtitle1">Select a movie:</Typography>
+      <Select
+        value={selectedMovie}
+        onChange={handleMovieChange}
+        displayEmpty
+        fullWidth
+        error={showSelectError}
+      >
+        <MenuItem value="" disabled>
+          Select a movie
+        </MenuItem>
+        {movies.map((title, index) => (
+          <MenuItem key={index} value={title}>
+            {title}
+          </MenuItem>
+        ))}
+      </Select>
+      {showSelectError && (
+        <Typography variant="subtitle2" style={{ color: "red" }}>
+          Select your movie
+        </Typography>
+      )}
+    </div>
   );
 }
 
